455719e50b6caa1fe8ab7390faf1ed6c209ee1a5
codingfanatic <lclarkerichard@yahoo.com>
2018-12-08 23:08:57 -0500

    Replacement lolshack.sh
    This is the lolshack.sh file to use after the installation. Instead of editing the lolshack file on the installation disk, users can copy this one to their lolshack folder to replace the existing one. It contains this code below:
    
    #!/bin/sh
    
    #Access Esc Menu from Select + Triangle on controller
    sleep 5s
    export PCSX_ESC_KEY=2
    killall ui_menu
    sleep 5s
    cd /data/AppData/sony/pcsx
    /usr/sony/bin/ui_menu --power-off-enable
    sync

---
+++ Post_install_lolshack/lolhack.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#Access Esc Menu from Select + Triangle on controller
+sleep 5s
+export PCSX_ESC_KEY=2
+killall ui_menu
+sleep 5s
+cd /data/AppData/sony/pcsx
+/usr/sony/bin/ui_menu --power-off-enable
+sync 
